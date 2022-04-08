22c2822995c32e931ab88be20e709c4e8d33afd9
morgadope <pedron.morgdo@gmail.com>
2022-04-07 23:09:31 -0300

    Commit inicial

---
+++ src/main/java/com/morgadope/api/ApiApplication.java
@@ -0,0 +1,15 @@
+package com.morgadope.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PatchMapping;
+
+@SpringBootApplication
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/morgadope/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morgadope.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
